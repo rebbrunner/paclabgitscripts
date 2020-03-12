cbf0affd0f055bfb3781552dd2a0427ff23ba447
Rebecca Brunner <brunner@bgsu.edu>
2020-03-12 17:25:31 -0400

    Update create script

--- create.sh
+++ create.sh
@@ -1,20 +1,28 @@
-# This script takes a series of arguements including a git url, folder path, and folder name.  The URL is used to clone to the folder 
-# path and folder directory and then the resulting repository is filtered using filter-repo.
+# This script takes a git url.  The URL is used to clone to the current directory.
 
-while test $# -gt 0
-do
-    URL="$1"
-    CLONE_PATH="$2"
-    FOLDER_NAME="$3"
-    echo "$FOLDER_NAME"
-    cd "$CLONE_PATH"
-    git clone "$URL" "$FOLDER_NAME"
-    cd "$FOLDER_NAME"
-    remote=$(git remote) && url=$(git remote get-url --push $remote)
-    git filter-repo --invert-paths --path-match data/
-    git filter-repo --invert-paths --path-match output/
-    git remote add origin "$url"
-    shift
-    shift
-    shift
-done
+url="$1"
+
+# Enable sparse checkout
+git init
+git config --local core.sparseCheckout true
+echo "**/*.java" > .git/info/sparse-checkout
+
+# Get objects
+git remote add origin "$url"
+git fetch --depth=1
+
+# Filter out unwanted objects
+remote=$(git remote) && url=$(git remote get-url --push $remote)
+git filter-repo --path-regex '^.*/*.java$'
+git remote add origin "$url"
+
+# Unpack objects
+cd .git
+mv objects/pack pack
+pack=$(ls pack | grep '.pack')
+git unpack-objects < pack/"$pack"
+rm -rf pack
+cd ../
+
+# Checkout working tree
+git checkout master
