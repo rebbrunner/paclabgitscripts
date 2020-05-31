c019f91b58349e6c815fc3ef5f36f2ca49183a8d
Rebecca Brunner <brunner@bgsu.edu>
2020-05-30 22:32:56 -0400

    Update eval pt 2 script

--- evalpt2.sh
+++ evalpt2.sh
@@ -1,65 +1,76 @@
 #!/bin/sh
 
+# Collects data every snapshot - might be overkill.  Justfication - naive approach is a collection of folders for each snapshot, our approach is one file.  If we wait until the end, in order to detect force pushes we will have to test every snapshot against every different folder in the naive approach, which means matching dates with snapshot names.
+
 url=$1
 # Format 'MON DAY YEAR' space delimited
 startday=$2
+startfolder=$3
 repo_install_path="/bin/filter-repo"
+zpool="zfs"
 
 export PATH="$PATH:${repo_install_path}"
 
-now=$(date | cut -d" " -f2,3,4 | tr -d '[:space:]' | tr -d ',')
+now=$(date +"%b %d %Y")
 name=$(echo "$url" | md5sum | cut -d" " -f1)
 
 # Naive Approach
-name_snapshot="${name}${now}"
-git clone "$url" "$name_snapshot"
-
-# Unpack objects if packed
-cd "$name_snapshot"/.git
-mv objects/pack pack
-pack=$(ls pack | grep '.pack')
-git unpack-objects < pack/"$pack"
-cd ../../
-
-# Get stats
-size=$(du -sb | cut -f 1)
-now=$(date | cut -d" " -f2,3,4 | tr -d ',')
-commits=$(git rev-list --count HEAD --since="${startday}")
-echo "${url},${commits},${size}" >> naive.csv
-
-# Our Approach
-if [ "$startday" = "$now" ]
-then
-    git clone "$url" "$name"
-    cd "$name"
-else
-    echo "false"
-    cd "$name"
-    git pull -X theirs --allow-unrelated-histories
+if [ "$startday" = "$now" ]; then
+    mkdir "${name}"
 fi
+cd "$name"
+git clone "$url" "$now"
 
 # Unpack objects if packed
-cd .git
+cd "${now}/.git"
 mv objects/pack pack
 pack=$(ls pack | grep '.pack')
 git unpack-objects < pack/"$pack"
 cd ../
 
-# Compare with Naive for Dangling Objects
-force_pushes=$(diff -r .git/objects ../$name_snapshot/.git/objects)
+# Get stats
+size=$(du -sb | cut -f 1)
+commits=$(git rev-list --count HEAD --since="${startday}")
+cd ../../
+echo "${url},${commits},${size}" >> naive.csv
+
+# Our Approach
+if [ "$startday" = "$now" ]
+then
+    zfs create "${zpool}/${name}"
+    cd "/${zpool}/${name}"
+    git clone "$url" .
+else
+    cd "/${zpool}/${name}"
+    git pull -X theirs --allow-unrelated-histories
+fi
+
+# Look for dangling objects
+dangling=$(git fsck --full)
 
 # Filter project
 remote=$(git remote) && url=$(git remote get-url --push $remote)
 git filter-repo --path-regex '^.*/*.java$' --force
 git remote add origin "$url"
 
+# Unpack objects
+cd .git
+mv objects/pack pack
+pack=$(ls pack | grep '.pack')
+git unpack-objects < pack/"$pack"
+cd ../
+
+# Snapshot
+zfs snapshot -r "${zpool}/${name}@${now}"
+
 # Get stats
 size=$(du -sb | cut -f 1)
 commits=$(git rev-list --count HEAD --since="${startday}")
-cd ../
+# start location
+cd "$startfolder"
 
 # Determine boolean value for force pushes
-if [ "$force_pushes" = "" ]; then
+if [ "$dangling" = "" ]; then
     force_pushes=0
 else
     force_pushes=1
